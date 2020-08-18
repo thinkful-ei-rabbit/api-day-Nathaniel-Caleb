448ef2e34ff6e64d6f3c679413fe1bd4a6aa9cab
NathanielGaffney <nate440@gmail.com>
2020-08-18 11:58:53 -0400

    exercise 2

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/nateCaleb';
+
+function getItems(){
+    return Promise.resolve('A successful response!');
+}
+
+export default {
+    getItems
+};
